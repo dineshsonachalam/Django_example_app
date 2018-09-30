7e813e724d5221943b29caf10db9a8780272dac3
dineshsonachalam <dineshsonachalam@gmail.com>
2018-09-30 15:21:54 +0530

    sample app

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post # Here . means current directory or current application
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
